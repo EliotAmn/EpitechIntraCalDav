f58b5c32b208068d1411cf01058c1b919c67123b
Eliot <eliot.amanieu.13@gmail.com>
2023-11-26 11:16:07 +0100

    Add default config file

---
+++ config.py
@@ -0,0 +1,8 @@
+CD_URL = "https://cloud.nextcloud/remote.php/dav/"
+CD_USERNAME = "cron_epitechcalendar"
+CD_PASSWORD = ""
+CD_CALENDAR_NAME = "IntraCalendar"
+INTRA_TOKEN = "" # (cookie "user" from intra.epitech.eu)
+SYNC_START = "2023-09-01"
+SYNC_END = "2024-12-31"
+ERR_WEBHOOK_URL = ""
